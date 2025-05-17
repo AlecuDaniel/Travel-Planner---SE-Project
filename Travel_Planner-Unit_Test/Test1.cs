ce4db7c687a18c3af94bba3eee5b0bafd531abc3
vicman1-droid <danialecu53@gmail.com>
2025-05-17 16:01:31 +0300

    Created a unit test for search function

---
+++ Travel_Planner-Unit_Test/Test1.cs
@@ -0,0 +1,27 @@
+﻿using Travel_Planner___SE_Project.Data;
+using Travel_Planner___SE_Project.Data.Repositories;
+using Travel_Planner___SE_Project.Services;
+namespace Travel_Planner_Unit_Test
+{
+    [TestClass]
+    public sealed class TravvelPlannerTest
+    {
+        [TestMethod]
+        public async Task GetDestinationList_By_Search_Name()
+        {
+
+            // Arrange
+            var fakeRepo = new FakeDestinationRepository();
+            var service = new DestinationService(fakeRepo);
+
+            // Act
+            var results = await service.SearchAsync("par");
+
+            // Assert
+            var resultList = results.ToList();
+            Assert.AreEqual(2, resultList.Count);
+            Assert.IsTrue(resultList.Any(d => d.Name == "Paris"));
+            Assert.IsTrue(resultList.Any(d => d.Name == "Parioli"));
+        }
+    }
+}
